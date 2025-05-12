5dd89f8b8fa5accac4fd71fb34b527bef1ba5a69
1901004 <166771485+1901004@users.noreply.github.com>
2025-05-12 12:35:58 +0900

    ステータスバーを表示

--- src/webview/main.tsx
+++ src/webview/main.tsx
@@ -1,18 +1,9 @@
 import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
 import { VSCodeButton } from "@vscode/webview-ui-toolkit/react";
-import * as vscode from "vscode";
 
 const main = () => {
   const [text, setText] = useState<string>("");
-
-  const statusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left, 100);
-
-  statusBarItem.text = "ShortCutTips";
-  statusBarItem.tooltip = "クリックするとメッセージを表示します";
-  statusBarItem.command = "demo-status-bar-item.helloWorld";
-
-  statusBarItem.show();
 
   return (
     <>
