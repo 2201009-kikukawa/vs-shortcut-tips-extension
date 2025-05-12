d5e59890db9b524faaff92880bb5d5fb31dfdd21
1901004 <166771485+1901004@users.noreply.github.com>
2025-05-12 11:39:48 +0900

    5/12わかってない

--- src/extension.ts
+++ src/extension.ts
@@ -1,50 +1,10 @@
 import { ExtensionContext, window } from "vscode";
 import { ViewProvider } from "./providers/ViewProvider";
-import * as vscode from "vscode";
 
 export function activate(context: ExtensionContext) {
-  const myViewProvider = new MyViewProvider(context.extensionUri);
-  context.subscriptions.push(vscode.window.registerWebviewViewProvider("myView", myViewProvider));
-  /*
-  // コマンド登録（package.json で定義した ID）
-  let disposable = vscode.commands.registerCommand("popup-button.showPopup", () => {
-    // ポップアップを表示
-    vscode.window
-      .showInformationMessage("テキストやファイルを複製\nctrl + c , ctrl + v", "動きを確認する")
-      .then((selection) => {
-        if (selection === "動きを確認する") {
-          vscode.window.showInformationMessage("ボタンを押したで");
-        }
-      });
-  });
+  const provider = new ViewProvider(context.extensionUri);
 
-  context.subscriptions.push(disposable);
-  */
-}
-export class MyViewProvider implements vscode.WebviewViewProvider {
-  constructor(private readonly extensionUri: vscode.Uri) {}
+  const sampleViewDisposable = window.registerWebviewViewProvider(ViewProvider.viewType, provider);
 
-  resolveWebviewView(
-    view: vscode.WebviewView,
-    _context: vscode.WebviewViewResolveContext,
-    _token: vscode.CancellationToken
-  ) {
-    view.webview.options = {
-      enableScripts: true,
-    };
-
-    view.webview.html = `
-      <html>
-      <body>
-        <button onclick="handleClick()">Click me!</button>
-        <script>
-          function handleClick() {
-            const vscode = acquireVsCodeApi();
-            vscode.postMessage({ command: 'hello' });
-          }
-        </script>
-      </body>
-      </html>
-    `;
-  }
+  context.subscriptions.push(sampleViewDisposable);
 }
