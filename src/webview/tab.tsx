cca74d29aed19e406222fa99ac3f2c7b78719719
1901004 <166771485+1901004@users.noreply.github.com>
2025-05-30 14:50:35 +0900

    タブ内容完成

--- src/webview/tab.tsx
+++ src/webview/tab.tsx
@@ -6,13 +6,31 @@ const Tab = () => {
   const gifSrc = rootEl?.getAttribute("data-gif");
 
   return (
-    <div>
-      {gifSrc ? (
-        <img src={gifSrc} alt="GIF" style={{ width: "300px" }} />
-      ) : (
-        <p>GIFが見つかりません</p>
-      )}
-    </div>
+    <>
+      <div>
+        <h1>キーボードショートカット詳細</h1>
+        <hr></hr>
+      </div>
+      <div className="field">
+        <div>
+          <h3>ショートカット名</h3>
+          <p>行の入れ替え</p>
+          <h3>概要</h3>
+          <p>選択中の行またはカーソルのある行を上下に移動させる機能です。</p>
+          <p>行のコピーではなく、実際に行を入れ替えるため、コードの並び替えや整理に便利です。</p>
+          <p>複数行選択中でもそのまま一括で移動できます。</p>
+          <h3>コマンド</h3>
+          <p>Option + ↑ or Option + ↓</p>
+        </div>
+        <div>
+          {gifSrc ? (
+            <img src={gifSrc} alt="GIF" style={{ width: "550px" }} />
+          ) : (
+            <p>GIFが見つかりません</p>
+          )}
+        </div>
+      </div>
+    </>
   );
 };
 
