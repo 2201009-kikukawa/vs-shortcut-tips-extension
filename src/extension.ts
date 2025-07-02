00d89b37931262cb2da156477053c2bc2ea47a62
1901004 <166771485+1901004@users.noreply.github.com>
2025-07-02 12:38:01 +0900

    5分ごとにポップアップを表示

--- src/extension.ts
+++ src/extension.ts
@@ -3,9 +3,12 @@ import { ViewProvider } from "./providers/TabProvider";
 import { getRandomShortcut } from "./utilities/getShortcut";
 import * as vscode from "vscode";
 
+let intervalTimer: NodeJS.Timeout | undefined;
+
 export function activate(context: ExtensionContext) {
   const provider = new ViewProvider(context, context.extensionUri);
 
+  //ステータスバー
   const statusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left, 100);
   statusBarItem.text = "ShortCutTips";
   statusBarItem.tooltip = "クリックするとメッセージを表示します";
@@ -25,4 +28,31 @@ export function activate(context: ExtensionContext) {
   });
 
   context.subscriptions.push(disposable);
+
+  //5分タイマー
+  const intervalInMinutes = 5;
+
+  intervalTimer = setInterval(() => {
+    const { shortcut } = getRandomShortcut();
+    const message = `${shortcut.name}:${shortcut.command}`;
+    vscode.window.showInformationMessage(message, "動きを確認する").then((selection) => {
+      if (selection === "動きを確認する") {
+        provider.openTabView(shortcut);
+      }
+    });
+  }, intervalInMinutes * 60 * 1000);
+
+  context.subscriptions.push({
+    dispose: () => {
+      if (intervalTimer) {
+        clearInterval(intervalTimer);
+      }
+    },
+  });
+}
+
+export function deactivate() {
+  if (intervalTimer) {
+    clearInterval(intervalTimer);
+  }
 }
