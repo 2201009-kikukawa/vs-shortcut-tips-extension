80a2db01b43b2aa031afc393a4681b986f6f4fb0
1901004 <166771485+1901004@users.noreply.github.com>
2025-07-09 11:23:46 +0900

    VsCodeButtonが上手くいっていない

--- src/webview/tab.tsx
+++ src/webview/tab.tsx
@@ -1,14 +1,17 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import ReactDOM from "react-dom/client";
 import { ShortcutProps } from "../const";
+import { VSCodeButton } from "@vscode/webview-ui-toolkit/react";
 
 const vscode = acquireVsCodeApi();
 
 const Tab = () => {
   const [data, setData] = useState<ShortcutProps | null>(null);
   const [isFavorite, setIsFavorite] = useState(false);
+  const buttonRef = useRef<HTMLElement | null>(null);
+
   useEffect(() => {
-    //VS Code 拡張へ「準備完了」を送信
+    // VS Code 拡張へ「準備完了」を送信
     vscode.postMessage({ type: "ready" });
 
     const listener = (event: MessageEvent) => {
@@ -23,32 +26,35 @@ const Tab = () => {
 
   if (!data) return <p>読み込み中...</p>;
 
+  const toggleFavorite = () => {
+    setIsFavorite((prev) => !prev);
+  };
+
   return (
     <>
-      <div className="field">
-        <div>
-          <h3>{data.name}</h3>
-          <p>{data.description}</p>
-          <p>
-            {data.command.split("+").map((key, index, array) => (
-              <span key={index}>
-                <kbd className="kbd-key">{key.trim()}</kbd>
-                {index < array.length - 1 && <span> + </span>}
-              </span>
-            ))}
-          </p>
+      <div className="box">
+        <div className="field">
+          <div>
+            <h3>{data.name}</h3>
+            <p>{data.description}</p>
+            <p>
+              {data.command.split("+").map((key, index, array) => (
+                <span key={index}>
+                  <kbd className="kbd-key">{key.trim()}</kbd>
+                  {index < array.length - 1 && <span> + </span>}
+                </span>
+              ))}
+            </p>
+          </div>
+          <div>
+            <VSCodeButton className="vscode-button" onClick={toggleFavorite} appearance="secondary">
+              {isFavorite ? "★ 保存済み" : "☆ ショートカットを保存"}
+            </VSCodeButton>
+          </div>
         </div>
-        <div>
-          <button className="favorite-button" onClick={() => setIsFavorite((prev) => !prev)}>
-            <span className={`favorite-star ${isFavorite ? "is-favorite" : ""}`}>
-              {isFavorite ? "★" : "☆"}
-            </span>
-            <span className="favorite-text">ショートカットを保存</span>
-          </button>
+        <div className="gif-field">
+          <img src={data.gif} alt="gif preview" className="gif-preview" />
         </div>
-      </div>
-      <div className="gif-field">
-        <img src={data.gif} alt="gif preview" className="gif-preview" />
       </div>
     </>
   );
